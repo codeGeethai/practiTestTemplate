0dfc621d1e6d1761e096508745bc188e35386bf0
Geethai Piriyan <webgeethai@yahoo.com>
2023-07-27 22:00:18 +1000

    updates to properties

--- src/test/java/com/example/PractiTestIntegration.java
+++ src/test/java/com/example/PractiTestIntegration.java
@@ -5,29 +5,47 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class PractiTestIntegration {
 
-    private final String API_TOKEN = "YOUR_PRACTITEST_API_TOKEN";
-    private final String BASE_URL = "https://api.practitest.com/api/v2/projects/PROJECT_ID.json"; // Add .json to the URL.
+    private String apiUrl;
+    private String apiToken;
+
+    public PractiTestIntegration() {
+        // Load properties from the properties file
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = PractiTestIntegration.class.getClassLoader().getResourceAsStream("practitest.properties")) {
+            properties.load(inputStream);
+            apiUrl = properties.getProperty("practitest.api.url");
+            apiToken = properties.getProperty("practitest.api.token");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load properties file.", e);
+        }
+    }
 
     public void createTestRun(String testName, String status) {
         RequestSpecification request = RestAssured.given()
-                .header("Authorization", "Basic " + API_TOKEN)
-                .contentType(ContentType.JSON); // Set the correct Content-Type header.
+                .header("Authorization", "Basic " + apiToken)
+                .contentType(ContentType.JSON);
 
-        Map<String, Object> requestBody = new HashMap<>();
-        Map<String, Object> data = new HashMap<>();
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("name", testName);
-        attributes.put("status", status);
-        data.put("type", "instances");
-        data.put("attributes", attributes);
-        requestBody.put("data", data);
+        String requestBody = "{\n" +
+                "    \"data\": {\n" +
+                "        \"type\": \"instances\",\n" +
+                "        \"attributes\": {\n" +
+                "            \"name\": \"" + testName + "\",\n" +
+                "            \"status\": \"" + status + "\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
 
-        Response response = request.body(requestBody).post(BASE_URL);
+        Response response = request.body(requestBody).post(apiUrl);
 
         if (response.getStatusCode() == 201) {
             System.out.println("Test run created successfully.");
